2477d72830d5c140bc11eab0874b80478430cbbe
DenisAleshkov <aleshkov592@gmail.com>
2020-05-21 03:01:25 +0300

    add dinamic dropdown and method logout

--- task-system/src/components/app/NavBar.vue
+++ task-system/src/components/app/NavBar.vue
@@ -9,19 +9,19 @@
         </div>
         <ul class="right hide-on-small-and-down">
           <li>
-            <a class="dropdown-trigger black-text" href="#" data-target="dropdown">
+            <a class="dropdown-trigger black-text" href="#" data-target="dropdown" ref="dropdown">
               USER NAME
               <i class="material-icons right">arrow_drop_down</i>
             </a>
             <ul id="dropdown" class="dropdown-content">
               <li>
-                <a href="#" class="black-text">
+                <router-link to="/profile" class="black-text">
                   <i class="material-icons">account_circle</i>Профиль
-                </a>
+                </router-link>
               </li>
               <li class="divider" tabindex="-1"></li>
               <li>
-                <a href="#" class="black-text">
+                <a href="#" class="black-text" @click.prevent="logout">
                   <i class="material-icons">assignment_return</i>Выйти
                 </a>
               </li>
@@ -30,4 +30,24 @@
         </ul>
       </div>
     </nav>
-</template>    
+</template>
+<script>
+  export default {
+    data: () => ({
+      dropdown: null
+    }),
+    mounted() {
+      this.dropdown = M.Dropdown.init(this.$refs.dropdown, {constrainWidth: false});
+    },
+    methods: {
+        logout(){
+          this.$router.push('/home?message=logout');
+      }
+    },
+    beforeDestroy() {
+      if(this.dropdown && this.dropdown.destroy){
+        this.dropdown.destroy();
+      }
+    }
+  }
+</script>
