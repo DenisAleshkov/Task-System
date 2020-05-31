9a6d5cbfc782873749b7b187bea51b074c278c60
DenisAleshkov <aleshkov592@gmail.com>
2020-05-31 17:42:51 +0300

    add rename accaunt ;

--- task-system/src/components/ProfileForm.vue
+++ task-system/src/components/ProfileForm.vue
@@ -3,17 +3,17 @@
     <div class="input-field">
       <input
           id="name"
-          type="text"
-          v-model.trim="name"
-          :class="{invalid: $v.name.$dirty && !$v.name.required}"    
+          type="text"  
+          v-model="name"
+          :class="{ invalid: ($v.name.$dirty && !$v.name.required)}"
       >
       <label for="name">Имя</label>
       <span
             class="helper-text invalid"
-           v-if="$v.name.$dirty && !$v.name.required"
+            v-if="$v.name.$dirty && !$v.name.required"
             >Введите ваше имя</span>
     </div>
-
+  </div>
     <button class="btn waves-effect waves-light" type="submit">
       Обновить
       <i class="material-icons right">send</i>
@@ -21,33 +21,36 @@
   </form>
 </template>
 <script>
-	import { required } from 'vuelidate/lib/validators'
-	export default{
-	data: () => ({
-      	name: ''
-    }),
-    validations: {
-      name:{required}
-    },
-    mounted() {
-      this.name = this.$store.getters.info.name
-      setTimeout(() => {
-      M.updateTextFields()
-   		}, 0)
-    },
-    methods: {
-      async submitHandler(){
-        if(this.$v.invalid){
+  import { mapGetters, mapActions } from 'vuex'
+  import { required } from 'vuelidate/lib/validators'
+ export default {
+  data: () => ({
+    name: ''
+  }),
+  validations: {
+      name: { required }
+  },
+  computed: {
+    ...mapGetters(['info'])
+  },
+  mounted() {
+    this.name = this.info.name
+
+    setTimeout(() => {
+       M.updateTextFields()
+     })
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      if(this.$v.$invalid) {
           this.$v.$touch()
           return
         }
-          const dataName = {
-            name: this.name
-          }
-          await this.$store.dispatch('updateName', dataName)
-          this.$message('Имя успешно обновлена')
-         
-      }
-    }  
-	}
+        try{
+          await this.updateInfo({name: this.name})
+        }catch(e){}
+    }
+  }
+ }
 </script>
